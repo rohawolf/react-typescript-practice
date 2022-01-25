ec305ed6e3691930d9ef0a9abf85891b0e68ea16
Mark Park <mark@soomgo.com>
2022-01-25 20:50:03 +0900

    add some config files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.ts',
+            '.tsx',
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
